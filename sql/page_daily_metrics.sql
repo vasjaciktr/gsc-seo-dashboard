41df1acb662208259d529fa344e39017d5a950f1
Vasyl Tsyktor <vasjaciktr@gmail.com>
2026-03-31 11:19:03 +0300

    Create page_daily_metrics.sql

---
+++ sql/page_daily_metrics.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `example-483110.gsc_analytics.page_daily_metrics` AS
+SELECT
+  date,
+  page,
+  SUM(clicks) AS clicks,
+  SUM(impressions) AS impressions,
+  SAFE_DIVIDE(SUM(clicks), SUM(impressions)) AS ctr,
+  SAFE_DIVIDE(SUM(position * impressions), SUM(impressions)) AS avg_position
+FROM `planeks-483110.gsc_analytics.gsc_raw_daily`
+GROUP BY date, page
